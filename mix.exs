8ef30403173e4e9e3d7ae5196d025d02512c51bb
Louis Pilfold <louis@lpil.uk>
2015-06-21 12:42:27 +0100

    Hexify!

--- mix.exs
+++ mix.exs
@@ -1,12 +1,23 @@
 defmodule Dogma.Mixfile do
   use Mix.Project
 
+  @version "0.0.1"
+
   def project do
     [
       app: :dogma,
-      version: "0.0.1",
+      version: @version,
       elixir: "~> 1.0",
       deps: deps,
+      name: "Dogma",
+      source_url: "https://github.com/lpil/dogma",
+      description: "A code style linter for Elixir, powered by shame.",
+      package: [
+        contributors: ["Louis Pilfold"],
+        licenses: ["MIT"],
+        links: %{ "GitHub" => "https://github.com/lpil/dogma" },
+        files: ~w(mix.exs lib README.md LICENCE)
+      ]
     ]
   end
 
@@ -17,7 +28,7 @@ defmodule Dogma.Mixfile do
   defp deps do
     [
       {:shouldi, only: :test},
-      {:mix_test_watch, "~> 0.1.1"},
+      {:mix_test_watch, "~> 0.1.1", only: :dev},
     ]
   end
 end
