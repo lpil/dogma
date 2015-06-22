01f8cd599ede21ed9e46fdd193b2aa141616723e
Louis Pilfold <louis@lpil.uk>
2015-06-22 23:26:32 +0100

    New Rule: WIP LiteralInCondition

---
+++ lib/dogma/rules/literal_in_condition.ex
@@ -0,0 +1,52 @@
+defmodule Dogma.Rules.LiteralInCondition do
+  @moduledoc """
+  A rule that disallows useless conditional statements that contain a literal
+  in place of a variable or predicate function.
+  """
+
+  alias Dogma.Script
+  alias Dogma.Error
+
+  def test(script) do
+    script |> Script.walk( &check_node(&1, &2) )
+  end
+
+
+  defp check_node({:if, meta, [predicate|_]} = node, errors) do
+    if predicate |> literal? do
+      errors = [error(meta[:line]) | errors]
+    end
+    {node, errors}
+  end
+  defp check_node(node, errors) do
+    {node, errors}
+  end
+
+
+  defp literal?(x) when is_atom       x do true end
+  defp literal?(x) when is_binary     x do true end
+  defp literal?(x) when is_bitstring  x do true end
+  defp literal?(x) when is_boolean    x do true end
+  defp literal?(x) when is_function   x do true end
+  defp literal?(x) when is_list       x do true end
+  defp literal?(x) when is_map        x do true end
+  defp literal?(x) when is_pid        x do true end
+  defp literal?(x) when is_port       x do true end
+  defp literal?(x) when is_number     x do true end
+  defp literal?(x) when is_reference  x do true end
+  defp literal?({:{}, _, _}) do
+    true
+  end
+  defp literal?(_) do
+    false
+  end
+
+
+  defp error(pos) do
+    %Error{
+      rule:     __MODULE__,
+      message:  "Literal value found in conditional",
+      position: pos,
+    }
+  end
+end
---
+++ test/dogma/rules/literal_in_condition_test.exs
@@ -0,0 +1,96 @@
+defmodule Dogma.Rules.LiteralInConditionTest do
+  use DogmaTest.Helper
+
+  alias Dogma.Rules.LiteralInCondition
+  alias Dogma.Script
+  alias Dogma.Error
+
+  def test(script) do
+    script |> Script.parse( "foo.ex" ) |> LiteralInCondition.test
+  end
+
+  with "a variable/function argument" do
+    with "if" do
+      setup context do
+        script = """
+        if feeling_tired do
+          have_an_early_night
+        end
+        """ |> test
+        %{ script: script }
+      end
+      should_register_no_errors
+    end
+
+    with "unless" do
+      setup context do
+        script = """
+        unless feeling_sleepy do
+          a_little_dance
+        end
+        """ |> test
+        %{ script: script }
+      end
+      should_register_no_errors
+    end
+
+    with "case" do
+      setup context do
+        script = """
+        case status do
+          :hyped -> run_like_the_wind
+          _      -> dawdle
+        end
+        """ |> test
+        %{ script: script }
+      end
+      should_register_no_errors
+    end
+  end
+
+
+  with "a literal argument" do
+    with "if" do
+      setup context do
+        script = """
+        if false do
+          i_will_never_run
+        end
+        """ |> test
+        %{ script: script }
+      end
+      should_register_errors [
+        %Error{
+          rule:     LiteralInCondition,
+          message:  "Literal value found in conditional",
+          position: 1,
+        }
+      ]
+    end
+
+    # with "unless" do
+    #   setup context do
+    #     script = """
+    #     unless [] do
+    #       useless_unless
+    #     end
+    #     """ |> test
+    #     %{ script: script }
+    #   end
+
+    # end
+
+    # with "case" do
+    #   setup context do
+    #     script = """
+    #     case 0 do
+    #       1 -> the_loneliest_number
+    #       _ -> go_to_guy
+    #     end
+    #     """ |> test
+    #     %{ script: script }
+    #   end
+
+    # end
+  end
+end
