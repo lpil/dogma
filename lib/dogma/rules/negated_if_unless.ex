20b003b42e06905617c5f23696daedefee18618a
Louis Pilfold <louis@lpil.uk>
2015-06-25 22:29:31 +0100

    New Rule: NegatedIfUnless
    Closes #13
    Closes #14
    
    I've had a lovely day, and I intend to have a lovely weekend.
    I hope the same is true for you!

---
+++ lib/dogma/rules/negated_if_unless.ex
@@ -0,0 +1,50 @@
+defmodule Dogma.Rules.NegatedIfUnless do
+  @moduledoc """
+  A rule that disallows the use of an if or unless with a negated predicate
+  """
+
+  alias Dogma.Script
+  alias Dogma.Error
+
+  def test(script) do
+    script |> Script.walk( &check_node(&1, &2) )
+  end
+
+  defp check_node({:if, [line: i], [{:not, _, _}, _]} = node, errors) do
+    err = error( :if, i )
+    {node, [err | errors]}
+  end
+  defp check_node({:if, [line: i], [{:!, _, _}, _]} = node, errors) do
+    err = error( :if, i )
+    {node, [err | errors]}
+  end
+
+  defp check_node({:unless, [line: i], [{:not, _, _}, _]} = node, errors) do
+    err = error( :unless, i )
+    {node, [err | errors]}
+  end
+  defp check_node({:unless, [line: i], [{:!, _, _}, _]} = node, errors) do
+    err = error( :unless, i )
+    {node, [err | errors]}
+  end
+
+  defp check_node(node, errors) do
+    {node, errors}
+  end
+
+
+  defp error(:unless, position) do
+    %Error{
+      rule: __MODULE__,
+      message: "Favour if over a negated unless",
+      position: position,
+    }
+  end
+  defp error(:if, position) do
+    %Error{
+      rule: __MODULE__,
+      message: "Favour unless over a negated if",
+      position: position,
+    }
+  end
+end
---
+++ test/dogma/rules/negated_if_unless_test.exs
@@ -0,0 +1,123 @@
+defmodule Dogma.Rules.NegatedIfUnlessTest do
+  use DogmaTest.Helper
+
+  alias Dogma.Rules.NegatedIfUnless
+  alias Dogma.Script
+  alias Dogma.Error
+
+  with "a non negated predicate" do
+    with "if" do
+      setup context do
+        script = """
+        if it_was_really_good do
+          boast_about_your_weekend
+        end
+        """ |> Script.parse( "foo.ex" )
+        %{
+          script: NegatedIfUnless.test( script )
+        }
+      end
+      should_register_no_errors
+    end
+
+    with "unless" do
+      setup context do
+        script = """
+        unless youre_quite_full do
+          have_another_slice_of_cake
+        end
+        """ |> Script.parse( "foo.ex" )
+        %{
+          script: NegatedIfUnless.test( script )
+        }
+      end
+      should_register_no_errors
+    end
+  end
+
+
+  with "a predicate negated with 'not'" do
+    with "if" do
+      setup context do
+        script = """
+        if not that_great do
+          make_the_best_of_it
+        end
+        """ |> Script.parse( "foo.ex" )
+        %{
+          script: NegatedIfUnless.test( script )
+        }
+      end
+      should_register_errors [
+        %Error{
+          message: "Favour unless over a negated if",
+          position: 1,
+          rule: NegatedIfUnless,
+        }
+      ]
+    end
+
+    with "unless" do
+      setup context do
+        script = """
+        unless not acceptable do
+          find_something_better
+        end
+        """ |> Script.parse( "foo.ex" )
+        %{
+          script: NegatedIfUnless.test( script )
+        }
+      end
+      should_register_errors [
+        %Error{
+          message: "Favour if over a negated unless",
+          position: 1,
+          rule: NegatedIfUnless,
+        }
+      ]
+    end
+  end
+
+  with "a predicate negated with '!'" do
+    with "if" do
+      setup context do
+        script = """
+        if ! that_great do
+          make_the_best_of_it
+        end
+        """ |> Script.parse( "foo.ex" )
+        %{
+          script: NegatedIfUnless.test( script )
+        }
+      end
+      should_register_errors [
+        %Error{
+          message: "Favour unless over a negated if",
+          position: 1,
+          rule: NegatedIfUnless,
+        }
+      ]
+    end
+
+    with "unless" do
+      setup context do
+        script = """
+        IO.puts "Hello, world!"
+        unless ! acceptable do
+          find_something_better
+        end
+        """ |> Script.parse( "foo.ex" )
+        %{
+          script: NegatedIfUnless.test( script )
+        }
+      end
+      should_register_errors [
+        %Error{
+          message: "Favour if over a negated unless",
+          position: 2,
+          rule: NegatedIfUnless,
+        }
+      ]
+    end
+  end
+end
