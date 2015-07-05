e113ec022565610f7370e2f1ae7c6329f9dfc85d
Louis Pilfold <louis@lpil.uk>
2015-07-05 14:25:47 +0100

    New Rule: ModuleAttributeName
    A rule that disallows module attribute names not in snake_case
    
    Closes #51

---
+++ lib/dogma/rules/module_attribute_name.ex
@@ -0,0 +1,33 @@
+defmodule Dogma.Rules.ModuleAttributeName do
+  @moduledoc """
+  A rule that disallows module attribute names not in snake_case
+  """
+
+  alias Dogma.Script
+  alias Dogma.Error
+  alias Dogma.Util.Name
+
+  def test(script) do
+    script |> Script.walk( &check_node(&1, &2) )
+  end
+
+
+  defp check_node({:@, meta, [{name, _, _}]} = node, errors) do
+    if name |> to_string |> Name.probably_not_snake_case? do
+      errors = [error( meta[:line] ) | errors]
+    end
+    {node, errors}
+  end
+  defp check_node(node, errors) do
+    {node, errors}
+  end
+
+
+  defp error(pos) do
+    %Error{
+      rule:     __MODULE__,
+      message:  "Module attribute names should be in snake_case",
+      position: pos,
+    }
+  end
+end
---
+++ test/dogma/rules/module_attribute_name_test.exs
@@ -0,0 +1,43 @@
+defmodule Dogma.Rules.ModuleAttributeNameTest do
+  use DogmaTest.Helper
+
+  alias Dogma.Rules.ModuleAttributeName
+  alias Dogma.Script
+  alias Dogma.Error
+
+  def test(script) do
+    script |> Script.parse( "foo.ex" ) |> ModuleAttributeName.test
+  end
+
+  with "valid module attribute names" do
+    setup context do
+      script = """
+      defmodule HelloWorld do
+        @hello_world 1
+        @hello 2
+      end
+      """ |> test
+      %{ script: script }
+    end
+    should_register_no_errors
+  end
+
+  with "a camelCase module attribute name" do
+    setup context do
+      script = """
+      defmodule :snake_case do
+        @helloWorld 1
+      end
+      """ |> test
+      %{ script: script }
+    end
+    should_register_errors [
+      %Error{
+        rule:     ModuleAttributeName,
+        message:  "Module attribute names should be in snake_case",
+        position: 2,
+      }
+    ]
+  end
+
+end
