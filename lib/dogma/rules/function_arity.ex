191262f7e57c890f181719c5d69970e9d6d1f3b2
steve.brazier <meadsteve@gmail.com>
2015-07-15 19:40:48 +0100

    Add rule for function arity Adds a rule that limits function arity to a configurable amount. This defaults to 4. It also allows for functions defined without arguments: def thing, do: "" def thing(), do: ""

---
+++ lib/dogma/rules/function_arity.ex
@@ -0,0 +1,45 @@
+defmodule Dogma.Rules.FunctionArity do
+  @moduledoc """
+  A rule that disallows functions with arity greater than 4 (configurable)
+  """
+
+  alias Dogma.Script
+  alias Dogma.Error
+
+  def test(script) do
+    test(script, max: 4)
+  end
+
+  def test(script, max: max) do
+    script
+      |> Script.walk( fn(node, errs) -> check_node(node, errs, max) end)
+  end
+
+  defp check_node({:def, _, _} = node, errors, max_arity) do
+    check_function(node, errors, max_arity)
+  end
+  defp check_node({:defp, _, _} = node, errors, max_arity) do
+    check_function(node, errors, max_arity)
+  end
+  defp check_node(node, errors, _max_arity) do
+    {node, errors}
+  end
+
+  defp check_function(node, errors, max_arity) do
+    {_, [line: line_number], [{_, _, function_args}, _]} = node
+    function_arity = Enum.count(function_args || [])
+    if (function_arity > max_arity) do
+      {node, [error(line_number, max_arity) | errors]}
+    else
+      {node, errors}
+    end
+  end
+
+  defp error(line_number, max_arity) do
+    %Error{
+      rule:     __MODULE__,
+      message:  "Function arity should be #{max_arity} or less",
+      position: line_number,
+    }
+  end
+end
---
+++ test/dogma/rules/function_arity_test.exs
@@ -0,0 +1,69 @@
+defmodule Dogma.Rules.FunctionArityTest do
+  use DogmaTest.Helper
+
+  alias Dogma.Rules.FunctionArity
+  alias Dogma.Script
+  alias Dogma.Error
+
+  def test(script) do
+    script |> Script.parse( "foo.ex" ) |> FunctionArity.test
+  end
+
+
+  with "valid arity" do
+    setup context do
+      script = """
+      def something() do
+      end
+
+      def something_else do
+      end
+
+      def point(a,b,c,d) do
+      end
+
+      def point(a,b,c,{d, e, f}) do
+      end
+      """ |> test
+      %{ script: script }
+    end
+    should_register_no_errors
+  end
+
+  with "invalid arity" do
+    setup context do
+      script = """
+      def point(a,b,c,d,e) do
+      end
+      """ |> test
+      %{ script: script }
+    end
+    should_register_errors [
+      %Error{
+        rule:     FunctionArity,
+        message:  "Function arity should be 4 or less",
+        position: 1,
+      }
+    ]
+  end
+
+  with "invalid arity based on custom config" do
+    setup context do
+      script = """
+      def point(a,b,c) do
+      end
+      """
+        |> Script.parse( "foo.ex" )
+        |> FunctionArity.test(max: 2)
+
+      %{ script: script }
+    end
+    should_register_errors [
+      %Error{
+        rule:     FunctionArity,
+        message:  "Function arity should be 2 or less",
+        position: 1,
+      }
+    ]
+  end
+end
