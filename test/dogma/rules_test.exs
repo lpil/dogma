0302e475bcd5bf8bdcd9222eebc1050c9c3df1d7
Louis Pilfold <louis@lpil.uk>
2015-07-16 22:16:34 +0100

    Ensure that all rules have a test/1 function
    Closes #60

---
+++ test/dogma/rules_test.exs
@@ -0,0 +1,23 @@
+defmodule Dogma.Rules.RulesTest do
+  use ShouldI
+
+  alias Dogma.Rules
+
+  # Let's ensure that all rules have a test function.
+  # They're not useful otherwise.
+
+  for rule <- Rules.Sets.All.list do
+
+    module = case rule do
+      {name}    -> name
+      {name, _} -> name
+    end
+
+    module_name = module |> Module.split |> hd |> String.to_atom
+
+    should "have test/1 function defined for rule #{module_name}" do
+      functions = Rules.unquote(module).__info__( :functions )
+      assert Enum.member?( functions, {:test, 1} )
+    end
+  end
+end
