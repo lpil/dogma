3eb48b529e9b2931f9c30ce1bae1e5a99a2421b8
Jack Noble <jacknoble@gmail.com>
2015-08-30 19:09:32 -0700

    Add tests for ASTNode/re-write literal?/1

--- lib/dogma/util/ast_node.ex
+++ lib/dogma/util/ast_node.ex
@@ -12,21 +12,16 @@ defmodule Dogma.Util.ASTNode do
       iex> Dogma.Util.ASTNode.literal?({:foo, [], Elixir})
       false
   """
-  def literal?(x) when is_atom       x do true end
-  def literal?(x) when is_binary     x do true end
-  def literal?(x) when is_bitstring  x do true end
-  def literal?(x) when is_boolean    x do true end
-  def literal?(x) when is_function   x do true end
-  def literal?(x) when is_list       x do true end
-  def literal?(x) when is_map        x do true end
-  def literal?(x) when is_pid        x do true end
-  def literal?(x) when is_port       x do true end
-  def literal?(x) when is_number     x do true end
-  def literal?(x) when is_reference  x do true end
-  def literal?({:{}, _, _}) do
-    true
-  end
-  def literal?(_) do
-    false
-  end
+  def literal?(x) when is_atom    x do true end
+  def literal?(x) when is_binary  x do true end
+  def literal?(x) when is_boolean x do true end
+  def literal?(x) when is_list    x do true end
+  def literal?(x) when is_number  x do true end
+  def literal?({_,_})               do true end
+  def literal?({:{}, _, _})         do true end
+  def literal?({:%{}, _, _})        do true end
+  def literal?({:sigil_s,_,_})      do true end
+  def literal?({:sigil_S,_,_})      do true end
+  def literal?({:sigil_w,_,_})      do true end
+  def literal?(_)                   do false end
 end
