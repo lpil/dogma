2b386bc8546bd7eceba1cfb5a36ecb509dad91b4
René Föhring <rf@bamaru.de>
2015-09-20 14:56:52 +0200

    Clean up code in Dogma.Util.ASTNode
    This cleans up some code to adhere to the style guide we are sometimes
    referring to.

--- lib/dogma/util/ast_node.ex
+++ lib/dogma/util/ast_node.ex
@@ -1,5 +1,5 @@
 defmodule Dogma.Util.ASTNode do
-  @moduledoc"""
+  @moduledoc """
   Utility functions for analyzing and categorizing AST nodes
   """
 
@@ -7,24 +7,27 @@ defmodule Dogma.Util.ASTNode do
 
   @doc """
   Returns true if the node x is a literal
+
+  ## Examples:
+
       iex> Dogma.Util.ASTNode.literal?( "foo" )
       true
 
       iex> Dogma.Util.ASTNode.literal?({:foo, [], Elixir})
       false
   """
-  def literal?(x) when is_atom    x do true end
-  def literal?(x) when is_binary  x do true end
-  def literal?(x) when is_boolean x do true end
-  def literal?(x) when is_list    x do true end
-  def literal?(x) when is_number  x do true end
-  def literal?({_,_})               do true end
-  def literal?({:{}, _, _})         do true end
-  def literal?({:%{}, _, _})        do true end
+  def literal?(x) when is_atom(x), do: true
+  def literal?(x) when is_binary(x), do: true
+  def literal?(x) when is_boolean(x), do: true
+  def literal?(x) when is_list(x), do: true
+  def literal?(x) when is_number(x), do: true
+  def literal?({_,_}), do: true
+  def literal?({:{}, _, _}), do: true
+  def literal?({:%{}, _, _}), do: true
 
   for char <- String.split(@sigil_chars, "", trim: true) do
     sigil_atom = String.to_atom("sigil_" <> char)
     def literal?({unquote(sigil_atom),_,_}), do: true
   end
-  def literal?(_) do false end
+  def literal?(_), do: false
 end
