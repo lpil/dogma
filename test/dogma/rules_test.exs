67693dd5deb829245a0485a09121c397f2ae192e
steve.brazier <meadsteve@gmail.com>
2015-10-11 12:58:12 +0100

    add test for calling rules.test with config

--- test/dogma/rules_test.exs
+++ test/dogma/rules_test.exs
@@ -1,6 +1,7 @@
 defmodule Dogma.RulesTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: false
   import Mock
+
   alias Dogma.Rules
   alias Dogma.Script
 
@@ -22,6 +23,20 @@ defmodule Dogma.RulesTest do
     end
   end
 
+  test "Config can be used to define the rules used" do
+    Application.put_env(:dogma, :rule_set, Dogma.RuleSets.FakeRuleSet)
+    single_script = %Script{}
+    expected_script_after_run = %Script{errors: [:always_fake_error]}
+
+    result = Rules.test([single_script], @formater_for_testing)
+
+    assert result == [expected_script_after_run]
+  end
+
+end
+
+defmodule Dogma.RuleSets.FakeRuleSet do
+  def rules, do: [{FakeRule}]
 end
 
 defmodule Dogma.Rule.FakeRule do
