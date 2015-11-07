0e21f4a4a73a7de0a62f937cb70beb9c5d1c3d2d
Tino Breddin <t@tolbrino.com>
2015-11-07 16:37:58 +0100

    Fixed parameter tests and  test

--- test/mix/tasks/dogma_test.exs
+++ test/mix/tasks/dogma_test.exs
@@ -10,19 +10,19 @@ defmodule Dogma.OptionParserTest do
   with ".parse_args" do
     with "empty args" do
       should "return nil and default formatter" do
-        assert parse_args([]) == {nil, @default_formatter}
+        assert parse_args([]) == {nil, @default_formatter, false}
       end
     end
 
     with "directories given" do
       should "return directory and default formatter" do
-        assert parse_args(["lib/foo"]) == {"lib/foo", @default_formatter}
+        assert parse_args(["lib/foo"]) == {"lib/foo", @default_formatter, false}
       end
 
       with "multiple directories given" do
         should "return first directory only" do
           parsed = parse_args(["lib/foo", "lib/bar"])
-          assert parsed == {"lib/foo", @default_formatter}
+          assert parsed == {"lib/foo", @default_formatter, false}
         end
       end
     end
@@ -31,22 +31,29 @@ defmodule Dogma.OptionParserTest do
       with "simple passed" do
         should "return simple formatter" do
           parsed = parse_args(["--format", "simple"])
-          assert parsed == {nil, Dogma.Formatter.Simple}
+          assert parsed == {nil, Dogma.Formatter.Simple, false}
         end
       end
 
       with "flycheck passed" do
         should "return flycheck formatter" do
           parsed = parse_args(["--format=flycheck"])
-          assert parsed == {nil, Dogma.Formatter.Flycheck}
+          assert parsed == {nil, Dogma.Formatter.Flycheck, false}
         end
       end
 
       with "unknown formatter passed" do
         should "return default formatter" do
           parsed = parse_args(["--format", "false"])
-          assert parsed == {nil, @default_formatter}
+          assert parsed == {nil, @default_formatter, false}
         end
+      end
+    end
+
+    with "noerrors option passed" do
+      should "return disabled error exit code" do
+          parsed = parse_args(["--noerrors"])
+          assert parsed == {nil, @default_formatter, true}
       end
     end
   end
