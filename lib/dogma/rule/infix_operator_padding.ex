234a54073c71d9dcade7f5cfd91e7f493b022423
Marcus Gartner <marcus@lob.com>
2015-11-10 23:52:02 -0800

    New rule: InfixOperatorPadding

---
+++ lib/dogma/rule/infix_operator_padding.ex
@@ -0,0 +1,156 @@
+use Dogma.RuleBuilder
+
+defrule Dogma.Rule.InfixOperatorPadding,
+  [fn_arrow_padding: false, elixir: ">= 1.1.0"] do
+  @moduledoc """
+  A rule that ensures that all infix operators, except the range operator `..`,
+  are surrounded by spaces.
+
+  This rule is only enabled for Elixir v1.1 or greater.
+
+  Good:
+
+      foo = bar
+
+      foo - bar
+
+      foo || bar
+
+      foo |> bar
+
+      foo..bar
+
+  Bad:
+
+      foo=bar
+
+      foo-bar
+
+      foo||bar
+
+      foo|>bar
+
+  By default, no space is required between the `fn` and `->` of an anonymous
+  function. A space can be required by setting the `fn_arrow_padding` option to
+  `true`.
+  """
+
+  @operators [
+    :comp_op,
+    :comp_op2,
+    :dual_op,
+    :mult_op,
+    :two_op,
+    :arrow_op,
+    :rel_op,
+    :rel_op2,
+    :and_op,
+    :or_op,
+    :match_op,
+    :in_match_op,
+    :assoc_op,
+    :stab_op,
+    :pipe_op
+  ]
+
+  @subtracters [
+    :number,
+    :identifier,
+    :")",
+    :")"
+  ]
+
+  @ignore_ops [
+    :-,
+    :..
+  ]
+
+  def test(rule, script) do
+    script.tokens
+    |> Enum.map(&normalize_token/1)
+    |> check_operators(rule)
+  end
+
+  defp normalize_token({a, {b, c, d}}), do: {a, b, c, d, nil}
+  defp normalize_token({a, {b, c, d}, e}), do: {a, b, c, d, e}
+  defp normalize_token({a, {b, c, d}, e, _}), do: {a, b, c, d, e}
+  defp normalize_token({a, {b, c, d}, e, _, _}), do: {a, b, c, d, e}
+
+  defp check_operators(tokens, rule, acc \\ [])
+
+  defp check_operators([], _rule, acc), do: Enum.reverse(acc)
+
+  defp check_operators([
+    {token, line, _, _, _},
+    {:identifier, line, _, _, _},
+    {:mult_op, line, _, _, :/}
+    | rest], rule, acc)
+  when token == :capture_op or token == :. do
+    check_operators(rest, rule, acc)
+  end
+
+  defp check_operators([
+    {token1, line, _, column, _},
+    {:dual_op, line, column, _, :-},
+    {token3, line, _, _, _}
+    | rest], rule, acc)
+  when (token1 in @subtracters or token1 == :")")
+  and (token3 in @subtracters or token3 == :"(") do
+    check_operators(rest, rule, [error(line) | acc])
+  end
+
+  defp check_operators([
+    {token1, line, _, _, _},
+    {:dual_op, line, _, column, :-},
+    {token3, line, column, _, _}
+    | rest], rule, acc)
+  when (token1 in @subtracters or token1 == :")")
+  and (token3 in @subtracters or token3 == :"(") do
+    check_operators(rest, rule, [error(line) | acc])
+  end
+
+  defp check_operators([
+    {:fn, line, _, column, _},
+    {:stab_op, line, column, _, _}
+    | rest], rule, acc) do
+
+    if rule.fn_arrow_padding do
+      check_operators(rest, rule, [error(line) | acc])
+    else
+      check_operators(rest, rule, acc)
+    end
+  end
+
+  for operator <- @operators do
+    defp check_operators([
+      {unquote(operator), line, _, column, value},
+      {token2, line, column, _, _}
+      | rest], rule, acc)
+    when not value in @ignore_ops and token2 != :eol do
+      check_operators(rest, rule, [error(line) | acc])
+    end
+  end
+
+  for operator <- @operators do
+    defp check_operators([
+      {_, line, _, column, _},
+      {unquote(operator), line, column, _, value}
+      | rest], rule, acc)
+    when not value in @ignore_ops do
+      check_operators(rest, rule, [error(line) | acc])
+    end
+  end
+
+  defp check_operators([_ | rest], rule, acc) do
+    check_operators(rest, rule, acc)
+  end
+
+  defp error(line) do
+    %Error{
+      rule:    __MODULE__,
+      message: "Infix operators should be surrounded by whitespace.",
+      line:    line,
+    }
+  end
+
+end
---
+++ test/dogma/rule/infix_operator_padding_test.exs
@@ -0,0 +1,151 @@
+defmodule Dogma.Rule.InfixOperatorPaddingTest do
+  use RuleCase, for: InfixOperatorPadding
+
+  defp infix_error(line) do
+    %Error{
+      rule: InfixOperatorPadding,
+      message: "Infix operators should be surrounded by whitespace.",
+      line: line
+    }
+  end
+
+  if Version.match?(System.version, @rule.elixir) do
+
+    test "does not error with padding around infix operators" do
+      script = """
+      1 + 2 - 2 * 4 = 1 / 2 || 3 < 6 >= 2 <= 1 && false
+      2 - (-3 - 4) - 2
+      a - 1
+      a - b
+      g <> h
+      1 == 2
+      [1 / 2]
+      %{:j => 1}
+      1 &&& 2
+      j |> thing
+      cond do true -> false end
+      ^x = 1
+      """ |> Script.parse!("")
+
+      assert [] == Rule.test(@rule, script)
+    end
+
+    test "does not error with unary operators" do
+      script = """
+      -1
+      b = -3
+        -3
+      !c
+      add(-1, -5)
+      foo -10, -1
+      """ |> Script.parse!("")
+
+      assert [] == Rule.test(@rule, script)
+    end
+
+    test "does not error with function arity" do
+      script = """
+      &length/1
+      &Task.await/1
+      &Dogma.Task.Thing.work/2
+      """ |> Script.parse!("")
+
+      assert [] == Rule.test(@rule, script)
+    end
+
+    test "does not error with a multi-line match" do
+      script = """
+      a =
+        1
+      """ |> Script.parse!("")
+
+      assert [] == Rule.test(@rule, script)
+    end
+
+    test "does not error with range operator" do
+      script = """
+      1..200
+      foo..bar
+      """ |> Script.parse!("")
+
+      assert [] == Rule.test(@rule, script)
+    end
+
+    test "errors without padding before infix operators" do
+      script = """
+      1+ 2
+      2- 1
+      3* 4
+      1= 1
+      a<= b
+      c&& d
+      e|| f
+      g<> h
+      i== 2
+      [1/ 2]
+      x + y+ z
+      2- (-3 - 4)
+      (-3 - 4)- 2
+      %{:j=> 1}
+      k== 1
+      j|> thing
+      cond do true-> false end
+      1&&& 2
+      """ |> Script.parse!("")
+
+      expected_errors =
+        script.lines
+        |> Enum.map(fn {line, _} -> line end)
+        |> Enum.map(&infix_error/1)
+      assert expected_errors == Rule.test(@rule, script)
+    end
+
+    test "errors without padding after infix operators" do
+      script = """
+      1 +2
+      2 -1
+      3 *4
+      1 =1
+      a <=b
+      c &&d
+      e ||f
+      g <>h
+      i ==2
+      [1 /2]
+      x + y +z
+      2 -(-3 - 4)
+      (-3 - 4) -2
+      %{:j =>1}
+      k ==1
+      j |>thing
+      cond do true ->false end
+      1 &&&2
+      """ |> Script.parse!("")
+
+      expected_errors =
+        script.lines
+        |> Enum.map(fn {line, _} -> line end)
+        |> Enum.map(&infix_error/1)
+      assert expected_errors == Rule.test(@rule, script)
+    end
+
+    test "allows fn-> by default" do
+      script = """
+      fn-> something end
+      """ |> Script.parse!("")
+
+      assert [] == Rule.test(@rule, script)
+    end
+
+    test "disallows fn-> by configuration" do
+      rule = %InfixOperatorPadding{fn_arrow_padding: true}
+      script = """
+      fn-> something end
+      """ |> Script.parse!("")
+
+      assert [infix_error(1)] == Rule.test(rule, script)
+    end
+
+  end
+
+end
