dee0dfce1e7401f8e195c32e04742aab7aaf4764
Louis Pilfold <louis@lpil.uk>
2016-01-13 23:16:47 +0000

    Update rule documentation generation

--- test/dogma/documentation/rules_list_test.exs
+++ test/dogma/documentation/rules_list_test.exs
@@ -1,17 +1,16 @@
 defmodule Dogma.Documentation.RuleListTest do
   use ShouldI
 
-  # alias Dogma.Documentation.RuleList
+  alias Dogma.Documentation.RuleList
 
-  @tag :skip
   should "have an up to date docs/rules.md file" do
-    # expected = RuleList.rules_doc
-    # actual = File.read! "docs/rules.md"
-    # assert actual == expected, """
-    # docs/rules.md is out of date.
+    expected = RuleList.rules_doc
+    actual = File.read! "docs/rules.md"
+    assert actual == expected, """
+    docs/rules.md is out of date.
 
-    # Run `mix run scripts/generate_documentation.exs` in order to regenerate
-    # the documentation.
-    # """
+    Run `mix run scripts/generate_documentation.exs` in order to regenerate
+    the documentation.
+    """
   end
 end
