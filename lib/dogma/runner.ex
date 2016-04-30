fe240175d4912cf75ae56826bacfab54d25860e8
Louis Pilfold <louis@lpil.uk>
2016-04-30 12:07:35 +0100

    Do not run tests on files with invalid syntax
    Closes #113
    Closes #181
    Closes #205

--- lib/dogma/runner.ex
+++ lib/dogma/runner.ex
@@ -4,8 +4,13 @@ defmodule Dogma.Runner do
   """
 
   alias Dogma.Rule
+  alias Dogma.Script
 
-  def run_tests(script, rules) when is_map(script) and is_list(rules) do
+  def run_tests(%Script{ valid?: false, errors: errors }, _rules) do
+    errors
+  end
+
+  def run_tests(%Script{} = script, rules) when is_list(rules) do
     rules
     |> Enum.filter(&(&1.enabled))
     |> Enum.map( &Rule.test(&1, script) )
--- test/dogma/runner_test.exs
+++ test/dogma/runner_test.exs
@@ -1,8 +1,9 @@
 defmodule Dogma.RunnerTest do
   use ExUnit.Case
 
-  alias Dogma.Runner
+  alias Dogma.Error
   alias Dogma.RuleSet.All
+  alias Dogma.Runner
   alias Dogma.Script
 
   test "run_tests/2 run the given rules" do
@@ -11,5 +12,26 @@ defmodule Dogma.RunnerTest do
       |> Script.parse!("foo.ex")
       |> Runner.run_tests(All.rules)
     assert [] == errors
+  end
+
+  test "syntax errors don't run tests or cause crashes" do
+    script = """
+    defmodule DoclessModule do
+      def foo) do # Syntax error here
+      end
+    end
+    """ |> Script.parse("")
+    refute script.valid?
+    results = Runner.run_tests( script, All.rules )
+    assert results == [
+      %Error{
+        line: 1,
+        message: {
+          ~s(unexpected token: "),
+          ~s(". "do" starting at line 1 is missing terminator "end")
+        },
+        rule: SyntaxError,
+      },
+    ]
   end
 end
