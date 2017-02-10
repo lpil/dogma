c36094898a483246d6c7a837002653e902d5cea9
Jon Rowe <hello@jonrowe.co.uk>
2017-02-10 11:24:12 +1100

    replace deprecated Behaviour module with @callback attribute see https://hexdocs.pm/elixir/Module.html

--- lib/dogma/rule_set.ex
+++ lib/dogma/rule_set.ex
@@ -3,10 +3,8 @@ defmodule Dogma.RuleSet do
   The RuleSet behaviour, used to assert the interface used by our RuleSets
   """
 
-  use Behaviour
-
   @doc """
   Returns the rules and configurations for the set.
   """
-  defcallback rules :: [%{}]
+  @callback rules() :: [%{}]
 end
